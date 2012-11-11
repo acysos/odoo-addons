779a0d851421691a7885df284f2b1a155051055e
acysos <info@acysos.com>
2012-11-11 18:51:42 +0100

    [ADD] acy_mrp_quantity_change_in_production: Allow to change the quantiry in a manufacturing order with it is in production

---
+++ acy_mrp_quantity_change_in_production/mrp.py
@@ -0,0 +1,72 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Acysos S.L. (http://acysos.com) All Rights Reserved.
+#                       Ignacio Ibeas <ignacio@acysos.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from datetime import datetime
+from osv import osv, fields
+from tools.translate import _
+import netsvc
+import time
+import tools
+
+class mrp_production(osv.osv):
+    _inherit = 'mrp.production'
+    
+    def action_compute_in_production(self, cr, uid, ids, new_qty, properties=[]):
+        """ Computes bills of material of a product.
+        @param properties: List containing dictionaries of properties.
+        @return: No. of products.
+        """
+
+        results = []
+        bom_obj = self.pool.get('mrp.bom')
+        prod_line_obj = self.pool.get('mrp.production.product.line')
+        workcenter_line_obj = self.pool.get('mrp.production.workcenter.line')
+        for production in self.browse(cr, uid, ids):
+            cr.execute('delete from mrp_production_product_line where production_id=%s', (production.id,))
+            cr.execute('delete from mrp_production_workcenter_line where production_id=%s', (production.id,))
+            bom_point = production.bom_id
+            bom_id = production.bom_id.id
+            if not bom_point:
+                bom_id = bom_obj._bom_find(cr, uid, production.product_id.id, production.product_uom.id, properties)
+                if bom_id:
+                    bom_point = bom_obj.browse(cr, uid, bom_id)
+                    routing_id = bom_point.routing_id.id or False
+                    self.write(cr, uid, [production.id], {'bom_id': bom_id, 'routing_id': routing_id})
+
+            if not bom_id:
+                raise osv.except_osv(_('Error'), _("Couldn't find bill of material for product"))
+
+            factor = new_qty * production.product_uom.factor_inv / bom_point.product_uom.factor
+            res = bom_obj._bom_explode(cr, uid, bom_point, factor / bom_point.product_qty, properties)
+
+            results = res[0]
+            results2 = res[1]
+            for line in results:
+                line['production_id'] = production.id
+                prod_line_obj.create(cr, uid, line)
+            for line in results2:
+                line['production_id'] = production.id
+                workcenter_line_obj.create(cr, uid, line)
+        return len(results)
+    
+mrp_production()
