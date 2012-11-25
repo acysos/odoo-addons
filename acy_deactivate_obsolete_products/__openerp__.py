7fed17b759be557b62ad1ee0b16ec8cef46d60e2
acysos <info@acysos.com>
2012-11-26 00:19:38 +0100

    [ADD] acy_deactivate_obsolete_products: Deactivate a product wich state 'Obsolete'

---
+++ acy_deactivate_obsolete_products/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Acysos S.L. (http://acysos.com) All Rights Reserved.
+#                       Ignacio Ibeas <ignacio@acysos.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : "Deactivate Obsolete products",
+    "version" : "1.0",
+    "author" : "Acysos S.L.",
+    "website" : "www.acysos.com",
+    "category" : "Generic Modules/Inventory Control",
+    "description": """When the state of a product is changed to Obsolete it's deactivate. If the state is changes again to other state it's activate\n
+    Add a new menu to show the deactivated products.""",
+    "license" : "GPL-3",
+    "depends" : [
+        "base",
+        "sale"
+        ],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" :['product_view.xml'],
+    "active": False,
+    "installable": True
+}
