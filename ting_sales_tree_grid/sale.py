3811f6ad685077969d5f8cfb36aa9771aac29419
acysos <info@acysos.com>
2013-04-22 20:03:33 +0200

    [BUG] ting_sales_tree_grid: tree view real stock

--- ting_sales_tree_grid/sale.py
+++ ting_sales_tree_grid/sale.py
@@ -73,7 +73,8 @@ class sale_order_line(osv.osv):
                     'message': warn_msg
                     }
             result['product_uom_qty'] = qty
-
+        
+        uom2 = False
         if uom:
             uom2 = product_uom_obj.browse(cr, uid, uom)
             if product_obj.uom_id.category_id.id != uom2.category_id.id:
@@ -127,6 +128,18 @@ class sale_order_line(osv.osv):
                 result['product_uos'] = False
                 result['product_uos_qty'] = qty
             result['th_weight'] = q * product_obj.weight        # Round the quantity up
+            
+        if not uom2:
+            uom2 = product_obj.uom_id
+        if (product_obj.type=='product') and (product_obj.virtual_available * uom2.factor < qty * product_obj.uom_id.factor) \
+          and (product_obj.procure_method=='make_to_stock'):
+            warning = {
+                'title': _('Not enough stock !'),
+                'message': _('You plan to sell %.2f %s but you only have %.2f %s available !\nThe real stock is %.2f %s. (without reservations)') %
+                    (qty, uom2 and uom2.name or product_obj.uom_id.name,
+                     max(0,product_obj.virtual_available), product_obj.uom_id.name,
+                     max(0,product_obj.qty_available), product_obj.uom_id.name)
+            }
 
         # get unit price
 
