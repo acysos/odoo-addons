63cc1475626aa27803e3b933a942924ebd35a9f8
acysos <info@acysos.com>
2013-08-14 17:47:43 +0200

    [ADD] ting_sales_tree_grid: migrated to OpenERP 6.1

---
+++ ting_sales_tree_grid/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution	
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#    Copyright (c) 2010 Acysos S.L. (http://acysos.com) All Rights Reserved.
+#                       Ignacio Ibeas <ignacio@acysos.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name" : "Sale Order Editable Tree Grid",
+    "version" : "2.0",
+    "author" : "Acysos S.L., Ting!",
+    "category": "Generic Modules/Sales & Purchases",
+    "depends" : ["base","sale"],
+    "init_xml" : [],
+    "description": """
+        Editable tree view with virtual and real stocks for sales orders. Update to 6.1 by Acysos S.L.
+    """,
+    'update_xml': ["sale_view.xml"],
+    'installable': True,
+    'active': False,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
