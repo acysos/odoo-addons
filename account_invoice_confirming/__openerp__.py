9e7c8675b1269a21907d05f5ff32f59f3e6b93de
acysos <info@acysos.com>
2015-01-09 20:49:42 +0100

    [ADD] account_confirming: simple confirming support. Invoice state.

---
+++ account_invoice_confirming/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+########################################################################
+#
+# @authors: Ignacio Ibeas <ignacio@acysos.com>
+# Copyright (C) 2014  Acysos S.L.
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see http://www.gnu.org/licenses.
+########################################################################
+
+{
+    'name': 'Account Invoice Confirming',
+    'version': '1.0',
+    'author': 'Acysos S.L.',
+    'category': 'Accounting',
+    'description': """
+Add workflow to control the invoice with payment confirming
+    """,
+    'website': 'http://www.acysos.com',
+    'images': [],
+    'depends': ['base', 'account', 'account_payment', 'account_voucher'],
+    'data': ['account_invoice_workflow.xml', 'account_invoice_view.xml'],
+    'js': [],
+    'qweb': [],
+    'css': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ account_invoice_confirming/account_invoice.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+########################################################################
+#
+# @authors: Ignacio Ibeas <ignacio@acysos.com>
+# Copyright (C) 2014  Acysos S.L.
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see http://www.gnu.org/licenses.
+########################################################################
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+
+class account_invoice(orm.Model):
+    _inherit = 'account.invoice'
+
+    _columns = {
+        'state': fields.selection([
+            ('draft','Draft'),
+            ('proforma','Pro-forma'),
+            ('proforma2','Pro-forma'),
+            ('open','Open'),
+            ('confirming','Pending Confirming'),
+            ('paid','Paid'),
+            ('cancel','Cancelled'),
+            ],'Status', select=True, readonly=True, track_visibility='onchange',
+            help=' * The \'Draft\' status is used when a user is encoding a new and unconfirmed Invoice. \
+            \n* The \'Pro-forma\' when invoice is in Pro-forma status,invoice does not have an invoice number. \
+            \n* The \'Open\' status is used when user create invoice,a invoice number is generated.Its in open status till user does not pay invoice. \
+            \n* The \'Pending Confirming\' status is used when the confirming is received but the payment is pending. \
+            \n* The \'Paid\' status is set automatically when the invoice is paid. Its related journal entries may or may not be reconciled. \
+            \n* The \'Cancelled\' status is used when user cancel invoice.'),
+        }
