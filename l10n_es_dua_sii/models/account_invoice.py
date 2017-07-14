e02d86e302d19d43875ac988e136f2e6108f1501
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-07-14 13:25:25 +0200

    [FIX] l10n_es_dua_sii: fix key 13

--- l10n_es_dua_sii/models/account_invoice.py
+++ l10n_es_dua_sii/models/account_invoice.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2017 Consultoría Informática Studio 73 S.L.
+# Copyright (c) 2017 Acysos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from osv import osv, fields
@@ -24,17 +25,20 @@ class AccountInvoice(osv.osv):
         """
         res = super(AccountInvoice, self)._get_invoices(
             cr, uid, company, invoice)
-        if res.get('FacturaRecibida', False) and invoice.is_dua:
-            res['FacturaRecibida']['TipoFactura'] = 'F5'
-            res['FacturaRecibida'].pop('FechaOperacion', None)
-            res['FacturaRecibida']['IDEmisorFactura'] = \
-                {'NIF': invoice.company_id.partner_id.vat[2:]}
-            res['IDFactura']['IDEmisorFactura'] = \
-                {'NIF': invoice.company_id.partner_id.vat[2:]}
-            res['FacturaRecibida']['Contraparte']['NIF'] = \
-                invoice.company_id.partner_id.vat[2:]
-            res['FacturaRecibida']['Contraparte']['NombreRazon'] = \
-                invoice.company_id.name
+        if res.get('FacturaRecibida', False):
+            if invoice.is_dua:
+                res['FacturaRecibida']['TipoFactura'] = 'F5'
+                res['FacturaRecibida'].pop('FechaOperacion', None)
+                res['FacturaRecibida']['IDEmisorFactura'] = \
+                    {'NIF': invoice.company_id.partner_id.vat[2:]}
+                res['IDFactura']['IDEmisorFactura'] = \
+                    {'NIF': invoice.company_id.partner_id.vat[2:]}
+                res['FacturaRecibida']['Contraparte']['NIF'] = \
+                    invoice.company_id.partner_id.vat[2:]
+                res['FacturaRecibida']['Contraparte']['NombreRazon'] = \
+                    invoice.company_id.name
+            if invoice.registration_key.code == '13':
+                res['FacturaRecibida']['TipoFactura'] = 'F6'
         return res
 
 AccountInvoice()
