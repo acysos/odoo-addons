885d57ce2deafd4669cf5dc6d5aa2a3bdc28dd7e
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-08-31 18:14:45 +0200

    [ADD] l10n_es_coop: PGCE for cooperatives

---
+++ l10n_es_coop/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Ignacio Ibeas Izquierdo <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Planes de cuentas españoles (según PGCE 2008) - Cooperativas",
+    "version": "10.0.1.0.0",
+    "author": "Acysos S.L.",
+    "website": 'https://www.acysos.com',
+    "category": "Localization/Account Charts",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es",
+        "account_balance_reporting",
+    ],
+    "data": [
+        "data/account_chart_template.xml",
+        "data/account_account_coop_pyme.xml",
+        "data/account_chart_template_post.xml",
+    ],
+    'installable': True,
+}
