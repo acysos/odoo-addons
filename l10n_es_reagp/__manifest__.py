9e9949f0763880499d50c99b3fb2668d01a77913
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-09-01 13:32:10 +0200

    [ADD] l10n_es_reagp: PGCE regimen especial de agricultura, ganaderia y pesca (REAGP)

---
+++ l10n_es_reagp/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Régimen especial de agricultura, ganadería y pesca",
+    "version": "10.0.1.0.0",
+    "category": "Localization/Account Charts",
+    "website": "https://www.acysos.com",
+    "author": "Acysos S.L.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_es"
+    ],
+    "data": [
+        "data/taxes_reagp.xml",
+        "data/fiscal_position_reagp.xml",
+        "data/fiscal_position_taxes_reagp.xml"
+    ],
+}
