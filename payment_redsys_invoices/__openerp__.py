ec022eee32e5d4a1f3270686866b2c184726115a
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-10-25 17:04:15 +0200

    [FIX] payment_redsys_invoices: fix version

--- payment_redsys_invoices/__openerp__.py
+++ payment_redsys_invoices/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Redsys Payment Acquirer - Invoices",
-    "version": "8.0.0.0.1",
+    "version": "0.0.0.1",
     'category': 'Hidden',
     'summary': 'Payment Acquirer: if exist add invoices in result return',
     "website": "https://www.acysos.com",
