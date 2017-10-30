e648dfda6a5a063232d033fba5d97c760484702d
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-10-30 23:30:00 +0100

    [FIX] l10n_es_account_balance_report_coop: change name

--- l10n_es_account_balance_report_coop/__manifest__.py
+++ l10n_es_account_balance_report_coop/__manifest__.py
@@ -3,8 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Informes de cuentas anuales españoles - "
-    "Entidades sin fines lucrativos",
+    "name": "Informes de cuentas anuales españoles - Cooperativas",
     "version": "8.0.0.1.0",
     "author": "Acysos S.L.",
     "contributors": [
