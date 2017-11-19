6423dd93cee482f19a58e98daa036c850d41aec8
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-11-19 21:00:19 +0100

    [ADD] l10n_es_aeat_sii_alava: sii para la hacienda foral de Álava/Araba

---
+++ l10n_es_aeat_sii_alava/README.rst
@@ -0,0 +1,52 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+========================================================================
+Suministro Inmediato de Información en el IVA - Pais Vasco - Álava/Araba
+========================================================================
+
+Módulo para la presentación inmediata del IVA, extensión para la provincia 
+de Álava/Araba en el País Vasco.
+http://www.araba.eus/cs/Satellite?c=Page&cid=1224025371656&language=es_ES&pagename=DiputacionAlava%2FPage%2FDPA_B_contenidoFinal
+
+Installation
+============
+
+Para instalar esté módulo necesita:
+
+#. Libreria Python Zeep, se puede instalar con el comando 'pip install zeep'
+#. Libreria Python Requests, se puede instalar con el comando 'pip install requests'
+#. Libreria pyOpenSSL, versión 0.15 o posterior
+
+Módulos necesario:
+* l10n_es_aeat_sii: https://www.odoo.com/apps/modules/8.0/l10n_es_aeat_sii/
+
+
+Usage
+=====
+
+Se utiliza igual que el módulo general de SII, pero presenta en la Hacienda
+Foral de Álava/Araba
+
+
+Known issues / Roadmap
+======================
+
+* Ninguna
+
+Credits
+=======
+
+Contributors
+------------
+
+* Ignacio Ibeas - Acysos S.L. <ignacio@acysos.com>
+
+
+Maintainer
+----------
+
+.. image:: https://acysos.com/logo.png
+   :alt: Acysos S.L.
+   :target: https://www.acysos.com
---
+++ l10n_es_aeat_sii_alava/models/account_invoice.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def _get_test_mode(self, port_name):
+        self.ensure_one()
+        if self.company_id.state_id.code == '01' and self.company_id.sii_test:
+            return port_name
+        else:
+            return super(AccountInvoice, self)._get_test_mode(port_name)
+
+    @api.multi
+    def _connect_wsdl(self, wsdl, port_name):
+        self.ensure_one()
+        company = self.company_id
+        if company.state_id.code == '01' and not company.sii_test:
+            client = self._connect_sii(wsdl)
+            client._default_service_name = 'siiService'
+            port_name = self._get_test_mode(port_name)
+            client._default_port_name = port_name
+            binding_name = '{'+wsdl+'}siiBinding'
+            url = self.env['ir.config_parameter'].get_param(
+                'l10n_es_aeat_sii.url_soap.01', False)
+            return client.create_service(binding_name, url)
+        else:
+            return super(AccountInvoice, self)._connect_wsdl(wsdl, port_name)
+
+    @api.multi
+    def _get_wsdl(self, key):
+        self.ensure_one()
+        if self.company_id.state_id.code == '01':
+            return self.env['ir.config_parameter'].get_param(key+'.01', False)
+        else:
+            return super(AccountInvoice, self)._get_wsdl(key)
