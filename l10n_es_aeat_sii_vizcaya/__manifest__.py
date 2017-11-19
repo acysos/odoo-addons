b70047487b523bc68f1082c96d3b59f880f1528e
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-11-19 21:04:32 +0100

    [ADD] l10n_es_aeat_sii_alava: sii para la hacienda foral de Vizcaya/Bizkaia

---
+++ l10n_es_aeat_sii_vizcaya/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Ignacio Ibeas Izquierdo <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Suministro Inmediato de Información - País Vasco - "
+    "Vizcaya/Bizkaia",
+    "version": "10.0.1.0.0",
+    "author": "Acysos S.L.",
+    "website": 'https://www.acysos.com',
+    "category": "Localization/Account Charts",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es",
+        "l10n_es_aeat_sii"
+    ],
+    "data": [
+        "data/ir_config_parameter.xml"
+    ],
+    'installable': True,
+}
