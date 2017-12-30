de8ecb29d5d1de50677c79e75948de6a8ab89f53
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-12-30 19:18:27 +0100

    [FIX] l10n_es_aeat_sii_vizcaya: url producción

--- l10n_es_aeat_sii_vizcaya/models/account_invoice.py
+++ l10n_es_aeat_sii_vizcaya/models/account_invoice.py
@@ -16,6 +16,37 @@ class AccountInvoice(models.Model):
             return super(AccountInvoice, self)._get_test_mode(port_name)
 
     @api.multi
+    def _connect_wsdl(self, wsdl, port_name):
+        self.ensure_one()
+        company = self.company_id
+        if company.state_id.code == 'BI' and company.sii_test:
+            client = self._connect_sii(wsdl)
+            client._default_service_name = 'siiService'
+            port_name = self._get_test_mode(port_name)
+            client._default_port_name = port_name
+            binding_name = '{'+wsdl+'}siiBinding'
+            url = False
+            if port_name == 'SuministroFactEmitidas':
+                url = self.env['ir.config_parameter'].get_param(
+                    'l10n_es_aeat_sii.url_soap_out.48', False)
+            if port_name == 'SuministroFactRecibidas':
+                url = self.env['ir.config_parameter'].get_param(
+                    'l10n_es_aeat_sii.url_soap_in.48', False)
+            if port_name == 'SuministroCobrosEmitidas':
+                url = self.env['ir.config_parameter'].get_param(
+                    'l10n_es_aeat_sii.url_soap_pr.48', False)
+            if port_name == 'SuministroPagosRecibidas':
+                url = self.env['ir.config_parameter'].get_param(
+                    'l10n_es_aeat_sii.url_soap_ps.48', False)
+            if url:
+                return client.create_service(binding_name, url)
+            else:
+                return super(AccountInvoice, self)._connect_wsdl(
+                    wsdl, port_name)
+        else:
+            return super(AccountInvoice, self)._connect_wsdl(wsdl, port_name)
+
+    @api.multi
     def _get_wsdl(self, key):
         self.ensure_one()
         if self.company_id.state_id.code == 'BI':
