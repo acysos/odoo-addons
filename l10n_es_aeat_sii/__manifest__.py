8dcd6cc65ab2aedf5441d4c606295a1443e21c9d
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2018-10-08 09:10:30 +0200

    [ADD] l10n_es_aeat_sii: add drop invoice and controls to resend

--- l10n_es_aeat_sii/__manifest__.py
+++ l10n_es_aeat_sii/__manifest__.py
@@ -7,7 +7,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "10.0.1.2.2",
+    "version": "10.0.1.3.0",
     "category": "Accounting & Finance",
     "website": "https://www.acysos.com",
     "author": "Acysos S.L.",
