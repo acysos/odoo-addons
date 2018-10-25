12127dc5c97ff9bc10eab3aed8dd1aa21a241372
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2018-10-25 22:51:19 +0200

    [ADD] account_payroll_import_base_spain: spanish configuration for xls account import payroll

---
+++ account_payroll_import_base_spain/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Alexander Ezquevo <alexander@acysos.com>
+# Copyright (c) 2018 Ignacio Ibeas Izquierdo <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account payroll import base XLS - Spain",
+    "version": "1",
+    "author": "Acysos S.L.",
+    "website": "www.acysos.com",
+    "contributors": [
+        'Alexander Ezquevo <alexander@acysos.com>',
+        'Ignacio Ibeas <ignacio@acysos.com>'],
+    "category": "",
+    "license": "AGPL-3",
+    "description": """
+    """,
+    "depends": [
+        "account_payroll_import_base",
+    ],
+    "data": [
+        "views/company_view.xml",
+    ],
+    'images': ['static/description/banner.jpg'],
+    "installable": True,
+}
