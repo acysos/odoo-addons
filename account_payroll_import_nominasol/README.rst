ec39991518e0155d912744f7b0752abf3081f4ad
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2018-10-29 23:51:17 +0100

    [ADD] account_payroll_import_nominasol

---
+++ account_payroll_import_nominasol/README.rst
@@ -0,0 +1,51 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+.. image:: https://img.shields.io/badge/github-Acysos-lightgray.png?logo=github
+    :target: https://github.com/acysos/odoo-addons/tree/11.0/fleet_database_extension
+    :alt: Acysos
+
+==============================================
+XLS account move import payroll base NominaSol
+==============================================
+
+This module import payroll account moves from NominaSol
+https://www.sdelsol.com/programa-nominas-nominasol/
+
+Depends
+=======
+
+* account_payroll_import_base
+* account_payroll_import_base_spain
+
+Usage
+=====
+
+The usage is explain in account_payroll_import_base
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/acysos/odoo-addons/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Ignacio Ibeas - Acysos S.L. <ignacio@acysos.com>
+* Alexander Ezquevo - Acysos S.L. <alexander@acysos.com>
+
+
+Maintainer
+----------
+
+.. image:: https://acysos.com/logo.png
+   :alt: Acysos S.L.
+   :target: https://www.acysos.com
---
+++ account_payroll_import_nominasol/models/import_xls.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Alexander Ezquevo <alexander@acysos.com>
+# Copyright (c) 2018 Ignacio Ibeas Izquierdo <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models, api, _
+from odoo.exceptions import Warning
+import base64
+import xlrd
+
+
+class PayrollImportXls(models.Model):
+    _inherit = 'payroll.import.xls'
+    
+    software = fields.Selection(selection_add=[('csi', 'CSI')])
+    
+    def _do_import_nominasol(
+            self, note, journal, company, worksheet, curr_row):
+        self.ensure_one()
+        moves = {}
+        acc_move_obj = self.env['account.move']
+        employee_obj = self.env['hr.employee']
+        number_col = 0
+        name_col = 3
+        col_640 = 4
+        col_476 = 11
+        col_irpf = 10
+        col_deduction = 6
+        col_total = 5 
+        note += str(worksheet.cell_value(curr_row, name_col)) + ' \n'
+        move_vals = {
+            'name': worksheet.cell_value(curr_row, name_col),
+            'journal_id': journal.id,
+            'ref': _('Payroll'),
+            'date': self.date
+        }
+        payroll_extid = worksheet.cell_value(curr_row, number_col)
+        employee = employee_obj.search([
+            ('payroll_extid', '=', payroll_extid)])
+        if not employee:
+            raise Warning(
+                _('Employee not found name %s') % (
+                    str(worksheet.cell_value(curr_row, name_col)))
+            )
+        credit64 = worksheet.cell_value(curr_row, col_640)
+        line64_vals = {
+            'account_id': company.account_expense.id,
+            'name': _('Payroll'),
+            'journal_id': journal.id,
+            'credit': 0,
+            'debit': credit64,
+            'ref': _('Payroll'),
+            'tax_ids': [(6, 0, company.tax_code_base.ids)]
+        }
+        context = self._context.copy()
+        context['journal_id'] = journal.id
+        debitIRPF = worksheet.cell_value(curr_row, col_irpf)
+        irpf_vals = {
+            'account_id': company.account_irpf.id,
+             'name': _('Payroll'),
+             'journal_id': journal.id,
+             'debit': 0,
+             'credit': debitIRPF,
+             'ref': _('Payroll'),
+             'tax_line_id': company.tax_code_amount.id
+        }
+        debit_total = worksheet.cell_value(curr_row, col_total)
+        if not employee.payroll_account:
+            raise Warning(
+                _('The employee %s has not payroll account') % (
+                    employee.payroll_account)
+            )
+        total_vals = {
+            'account_id': employee.payroll_account.id,
+            'name': _('Payroll'),
+            'journal_id': journal.id,
+            'debit': 0,
+            'credit': debit_total,
+            'ref': _('Payroll')
+            }
+        debit_ss = worksheet.cell_value(curr_row, col_476)
+        ss_vals = {'account_id': company.account_ss.id,
+                   'name': _('Payroll'),
+                   'journal_id': journal.id,
+                   'debit': 0,
+                   'credit': debit_ss,
+                   'ref': _('Payroll')
+                   }
+        debit_deduction = worksheet.cell_value(curr_row, col_deduction)
+        deduction_vals = {'account_id': company.account_deduction.id,
+                   'name': _('Payroll'),
+                   'journal_id': journal.id,
+                   'debit': 0,
+                   'credit': debit_deduction,
+                   'ref': _('Payroll')
+                   }
+        move_vals['line_ids'] = [
+            (0, 0, line64_vals), (0, 0, irpf_vals), (0, 0, total_vals),
+            (0, 0, ss_vals), (0, 0, deduction_vals)]
+        try:
+            new_move = acc_move_obj.create(move_vals)
+        except Exception as fault:
+            raise Warning(
+                fault.name + ' ' + move_vals['name']
+            )
+        return note, new_move
