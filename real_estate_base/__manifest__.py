2d8b7c9106f2459a08b0a1e006e17ca62f512b17
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2018-12-01 12:28:08 +0100

    [FIX] real_estate: readme update

--- real_estate_base/__manifest__.py
+++ real_estate_base/__manifest__.py
@@ -38,18 +38,15 @@
          'account', 
          'base_vat',
          'crm_phonecall'
-#        "document_ftp",
         ],
     "init_xml" : [],
     "demo_xml" : [],
     "data" :[
         'security/real_estate_security.xml',
         'security/ir.model.access.csv',
-#        'report/generic_list.xml',
         'views/top_view.xml',
         'views/partner_view.xml',
         'data/top_sequence.xml',
-#        'data/document_data.xml',
         'views/company_view.xml',
         'views/res_user.xml',
         'data/rental_agreement_sequence.xml',
