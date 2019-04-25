c030ffb09de189d8a51e4e5e51434d85dd799f1a
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2019-04-25 17:14:20 +0200

    [MIG] account_payroll_import_base_spain

---
+++ account_payroll_import_base_spain/models/company.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Alexander Ezquevo <alexander@acysos.com>
+# Copyright (c) 2018 Ignacio Ibeas Izquierdo <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields
+
+
+class res_company(models.Model):
+    _inherit = 'res.company'
+
+    account_expense = fields.Many2one(
+        comodel_name='account.account', string='Account Expense',
+        required=True)
+    account_irpf = fields.Many2one(
+        comodel_name='account.account', string='Account IRPF',
+        required=True)
+    account_ss = fields.Many2one(
+        comodel_name='account.account', string='Account SS',
+        required=True)
+    account_deduction = fields.Many2one(
+        comodel_name='account.account', string='Account Deduction',
+        required=True)
+    tax_code_base = fields.Many2many(
+        comodel_name='account.tax', string='Tax code base',
+        required=True)
+    tax_code_amount = fields.Many2one(
+        comodel_name='account.tax', string='Tax code amount',
+        required=True)
