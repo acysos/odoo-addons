9a410a6a6786090ee9bf00015717dcfaa052775b
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2019-05-28 22:02:54 +0200

    [ADD] l10n_es_aeat_certificate, l10n_es_aeat_soap, l10n_es_aeat_partner_check

---
+++ l10n_es_aeat_soap/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Acysos S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'AEAT - SOAP Webservice',
+    'author': 'Acysos S.L.',
+    'website': 'https://www.acysos.com',
+    'category': 'Accounting',
+    'version': '11.0.0.1.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_es_aeat_certificate'
+    ],
+    'data': [
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+}
