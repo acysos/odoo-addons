65dc064cbe6beeddb0f2ffaedeb91bcde90e7907
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2019-08-07 13:19:05 +0200

    [ADD] l10n_es_aeat_sii: changes from 11.0 to 10.0

--- l10n_es_aeat_sii/models/account_invoice.py
+++ l10n_es_aeat_sii/models/account_invoice.py
@@ -351,10 +351,6 @@ class AccountInvoice(models.Model):
             taxes_total = -taxes['total_excluded']
         else:
             taxes_total = taxes['total_excluded']
-        tax_sii = {
-            "TipoImpositivo": tax_type,
-            "BaseImponible": taxes_total
-        }
         taxes_amount = taxes['taxes'][0]['amount']
         if (self.currency_id !=
                 self.company_id.currency_id):
@@ -363,6 +359,10 @@ class AccountInvoice(models.Model):
                 taxes_total, self.company_id.currency_id)
             taxes_amount = currency_id.compute(
                 taxes['taxes'][0]['amount'], self.company_id.currency_id)
+        tax_sii = {
+            "TipoImpositivo": tax_type,
+            "BaseImponible": taxes_total
+        }
         if tax_line_req:
             tipo_recargo = tax_line_req['percentage']
             cuota_recargo = tax_line_req['taxes'][0]['amount']
@@ -579,16 +579,16 @@ class AccountInvoice(models.Model):
                             type_breakdown[
                                 'PrestacionServicios']['Sujeta']['NoExenta'][
                                 'DesgloseIVA']['DetalleIVA'] = []
-                            tax_type = tax_line.amount * 100
-                            if str(tax_type) not in taxes_to:
-                                taxes_to[str(tax_type)] = \
-                                    self._get_sii_tax_line(
-                                        tax_line, line,
-                                        line.invoice_line_tax_ids)
-                            else:
-                                taxes_to = self._update_sii_tax_line(
-                                    taxes_to, tax_line, line,
+                        tax_type = tax_line.amount * 100
+                        if str(tax_type) not in taxes_to:
+                            taxes_to[str(tax_type)] = \
+                                self._get_sii_tax_line(
+                                    tax_line, line,
                                     line.invoice_line_tax_ids)
+                        else:
+                            taxes_to = self._update_sii_tax_line(
+                                taxes_to, tax_line, line,
+                                line.invoice_line_tax_ids)
         if len(taxes_f) > 0:
             for key, line in taxes_f.iteritems():
                 if self.type == 'out_refund' and self.refund_type == 'I':
@@ -631,21 +631,26 @@ class AccountInvoice(models.Model):
                     'Sujeta']['NoExenta']['DesgloseIVA'][
                     'DetalleIVA'].append(line)
         if 'DesgloseTipoOperacion' in taxes_sii:
-            if 'Entrega' in taxes_sii['DesgloseTipoOperacion']:
-                if 'Sujeta' in taxes_sii['DesgloseTipoOperacion']['Entrega']:
+            if 'Entrega' in taxes_sii['DesgloseTipoOperacion'] or \
+                    'PrestacionServicios' in taxes_sii['DesgloseTipoOperacion']:
+                if 'Entrega' in taxes_sii['DesgloseTipoOperacion']:
+                    dt_key = 'Entrega'
+                else:
+                    dt_key = 'PrestacionServicios'
+                if 'Sujeta' in taxes_sii['DesgloseTipoOperacion'][dt_key]:
                     if 'Exenta' in taxes_sii['DesgloseTipoOperacion'][
-                            'Entrega']['Sujeta']:
+                            dt_key]['Sujeta']:
                         if 'DetalleExenta' in taxes_sii[
-                            'DesgloseTipoOperacion']['Entrega']['Sujeta'][
+                            'DesgloseTipoOperacion'][dt_key]['Sujeta'][
                                 'Exenta']:
                             if 'BaseImponible' in taxes_sii[
-                                'DesgloseTipoOperacion']['Entrega']['Sujeta'][
+                                'DesgloseTipoOperacion'][dt_key]['Sujeta'][
                                     'Exenta']['DetalleExenta']:
                                 taxes_sii[
-                                'DesgloseTipoOperacion']['Entrega']['Sujeta'][
+                                'DesgloseTipoOperacion'][dt_key]['Sujeta'][
                                     'Exenta']['DetalleExenta'][
                                         'BaseImponible'] = round(taxes_sii[
-                                            'DesgloseTipoOperacion']['Entrega'][
+                                            'DesgloseTipoOperacion'][dt_key][
                                                 'Sujeta']['Exenta'][
                                                     'DetalleExenta'][
                                                         'BaseImponible'], 2
@@ -1243,6 +1248,7 @@ class AccountInvoice(models.Model):
     @api.multi
     def _drop_invoice(self):
         """ Drop invoice from SII"""
+        queue_obj = self.env['queue.job'].sudo()
         for invoice in self.filtered(lambda i: i.state in ['cancel']):
             sii_map = invoice._get_sii_map()
             company = invoice.company_id
