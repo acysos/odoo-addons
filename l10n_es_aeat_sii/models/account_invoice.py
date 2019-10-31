51c02f6efe1a73fd78348f4afc273725717d2c09
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2019-10-31 23:53:12 +0100

    [FIX] l10n_es_aeat_sii: fix isp refund

--- l10n_es_aeat_sii/models/account_invoice.py
+++ l10n_es_aeat_sii/models/account_invoice.py
@@ -707,14 +707,14 @@ class AccountInvoice(models.Model):
                     if line.get('CuotaSoportada', False):
                         line['CuotaSoportada'] = \
                             -round(line['CuotaSoportada'], 2)
-                        line['BaseImponible'] = -round(
-                            line['BaseImponible'], 2)
+                    line['BaseImponible'] = -round(
+                        line['BaseImponible'], 2)
                 else:
                     if line.get('CuotaSoportada', False):
                         line['CuotaSoportada'] = \
                             round(line['CuotaSoportada'], 2)
                         line['BaseImponible'] = round(line['BaseImponible'], 2)
-                line['BaseImponible'] = round(line['BaseImponible'], 2)
+                    line['BaseImponible'] = round(line['BaseImponible'], 2)
                 if line.get('TipoImpositivo', False):
                     line['TipoImpositivo'] = round(line['TipoImpositivo'], 2)
                 taxes_sii['InversionSujetoPasivo']['DetalleIVA'].append(line)
