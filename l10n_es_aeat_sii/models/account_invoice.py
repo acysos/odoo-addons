97096579bc55c1aa75a424c0d0471306fdd74126
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2019-12-01 01:22:47 +0100

    [FIX] l10n_es_aeat_sii: fix registration key

--- l10n_es_aeat_sii/models/account_invoice.py
+++ l10n_es_aeat_sii/models/account_invoice.py
@@ -689,14 +689,9 @@ class AccountInvoice(models.Model):
                         line['CuotaSoportada'] = \
                             abs(round(line['CuotaSoportada'], 2))
                         line['BaseImponible'] = round(line['BaseImponible'], 2)
-                if self.registration_key.code in ['03', '05', '09', '16']:
-                    line['CuotaSoportada'] = \
-                            abs(round(0, 2))
                 line['BaseImponible'] = round(line['BaseImponible'], 2)
                 if line.get('TipoImpositivo', False):
                     line['TipoImpositivo'] = round(line['TipoImpositivo'], 2)
-                    if self.registration_key.code in ['03', '05', '09', '16']:
-                        line['TipoImpositivo'] = round(0, 2)
                 taxes_sii['DesgloseIVA']['DetalleIVA'].append(line)
         if len(taxes_isp) > 0:
             for key, line in taxes_isp.iteritems():
