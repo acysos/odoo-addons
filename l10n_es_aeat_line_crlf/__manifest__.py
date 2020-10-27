a0235907d1760cf000c353ecbeb3e0d77f0cb60f
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2020-10-27 18:06:20 +0100

    [ADD] l10n_es_aeat_line_crlf

---
+++ l10n_es_aeat_line_crlf/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Ignacio Ibeas <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "AEAT Base CRLF (Ej. Navarra)",
+    "version": "11.0.1.2.1",
+    "category": "Accounting & Finance",
+    "website": "https://www.acysos.com",
+    "author": "Acysos S.L.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_es_aeat"
+    ],
+    "data": [
+        'views/aeat_export_configuration_view.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+}
