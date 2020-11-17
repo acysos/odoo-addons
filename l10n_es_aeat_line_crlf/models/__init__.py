43f49690fe8e7202258b172f3b3619a5fa64b179
ae-landoo <ae@landoo.es>
2020-11-17 14:38:00 +0100

    migration l10n_es_aeat_line_crlf from 12.0 to 13.0

---
+++ l10n_es_aeat_line_crlf/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Ignacio Ibeas <ignacio@acysos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import l10n_es_aeat_export_config_line
